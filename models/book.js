77c13bb53ac583dd6bce84bbbee8edc1b4433053
Ryan Leach <leachrp@comcast.net>
2019-12-25 17:25:26 -0800

    Add search for books by query and filter by reading level query

--- models/book.js
+++ models/book.js
@@ -12,7 +12,20 @@ const bookSchema = new mongoose.Schema({
   }
 });
 
+// ISBN 020.__.a exact search
+// Call number 852.__.h + 852__.i  exact search
+// Barcode 852.__.p exact search
+// Title 245.10.a fuzzy search
+// Author 100.1_.a fuzzy search
+// Subject 650._0.a fuzzy search
+// Publisher 264._1.b fuzzy search
+// Genre 655._7.a fuzzy search
+// Series 490.1_.a fuzzy search
+// Reading level 526.0_.c filter
+
+// Anywhere search index
 bookSchema.index({
+  "fields.020.__.a": "text",
   "fields.100.1_.a": "text",
   "fields.245.10.a": "text",
   "fields.245.10.c": "text",
@@ -20,9 +33,27 @@ bookSchema.index({
   "fields.490.1_.a": "text",
   "fields.520.__.a": "text",
   "fields.650._0.a": "text",
+  "fields.650._1.a": "text",
   "fields.655._7.a": "text",
   "fields.700.1_.a": "text",
-  "fields.800.1_.a": "text"
+  "fields.800.1_.a": "text",
+  "fields.852.__.h": "text",
+  "fields.852.__.p": "text"
+});
+
+// ISBN search index
+bookSchema.index({
+  "fields.020.__.a": -1
+});
+
+// Call Number Index 1
+bookSchema.index({
+  "fields.852.__.h": -1
+});
+
+// Call Number Index 2
+bookSchema.index({
+  "fields.852.__.i": -1
 });
 
 const Book = mongoose.model("Book", bookSchema);
--- routes/books.js
+++ routes/books.js
@@ -9,30 +9,128 @@ const router = express.Router();
 //   res.redirect("/");
 // }
 
+async function searchByQuery(query, field, subfieldA, subfieldB) {
+  books = await Book.find({ $text: { $search: `\"${query}\"` } });
+  let filteredBooks = [];
+  for (let i = 0; i < books.length; i++) {
+    for (let j = 0; j < books[i].fields.length; j++) {
+      if (books[i].fields[j][field]) {
+        if (books[i].fields[j][field][subfieldA]) {
+          if (
+            books[i].fields[j][field][subfieldA][subfieldB]
+              .toLowerCase()
+              .includes(query.toLowerCase())
+          ) {
+            filteredBooks.push(books[i]);
+          }
+        }
+      }
+    }
+  }
+  books = filteredBooks;
+  return books;
+}
+
 // Search by:
-// Anywhere search
-// Barcode 852.__.p exact
-// Title 245.10.a search
-// Author 100.1_.a search
-// Subject 650._0.a search
-// Reading level 526.0_.c exact
-// Publisher 264._1.b search
-// Genre 655._7.a search
-// Series 490.1_.a search
-// ISBN 020.__.a exact
-// Call number 852.__.h + 852__.i  exact
+// Anywhere search done
+// Barcode 852.__.p exact search done
+// ISBN 020.__.a exact search done
+// Call number 852.__.h + 852__.i  exact search done
+// Title 245.10.a search *Test once all documents are loaded. Might need to replace with RegEx*
+// Author 100.1_.a search done
+// Subject 650._0.a search done
+// Publisher 264._1.b search done
+// Genre 655._7.a search done
+// Series 490.1_.a search done
+// Reading level 526.0_.c filter done
 
 //Full Text Search Fields: 100.1_.a, 245.10.a, 245.10.c, 264._1.b, 490.1_.a, 520.__.a, 650._0.a, 655._7.a, 700.1_.a, 800.1_.a,
 
-// Get all books
+// Search for books
 router.get("/", async (req, res) => {
-  const books = await Book.find({
-    $text: { $search: "Police" }
-    // "fields.245.10.a": "Dog Man unleashed /"
-  });
-  if (req.query.any != "undefined") {
-    const result = Book.find();
+  let books;
+
+  // Return all books in collection
+  if (req.query.any === "*") {
+    books = await Book.find();
+    // Search any text fields for query and sort by textScore
+  } else if (req.query.any) {
+    books = await Book.find(
+      {
+        $text: { $search: `\"${req.query.any}\"` }
+      },
+      { score: { $meta: "textScore" } }
+    ).sort({ score: { $meta: "textScore" } });
+    // Search for books by ISBN number
+  } else if (req.query.isbn) {
+    books = await Book.find({ "fields.020.__.a": req.query.isbn });
+    // Search for books by Call Number
+  } else if (req.query.call) {
+    let callNumberArray = req.query.call.split(" ");
+    books = await Book.find({
+      "fields.852.__.h": `${callNumberArray[0]}`,
+      "fields.852.__.i": `${callNumberArray[1]}`
+    });
+    // Search for books by Barcode
+  } else if (req.query.bar) {
+    books = await Book.find({
+      "fields.852.__.p": req.query.bar
+    });
+    // Search for books by Title
+  } else if (req.query.title) {
+    books = await searchByQuery(req.query.title, "245", "10", "a");
+    // Search for books by Author
+  } else if (req.query.author) {
+    books = await searchByQuery(req.query.author, "100", "1_", "a");
+    // Search for books by Publisher
+  } else if (req.query.publisher) {
+    books = await searchByQuery(req.query.publisher, "264", "_1", "b");
+    // Search for books by Series
+  } else if (req.query.series) {
+    books = await searchByQuery(req.query.series, "490", "1_", "a");
+    // Search for books by Genre
+  } else if (req.query.genre) {
+    let genreBooks = await searchByQuery(req.query.genre, "655", "_7", "a");
+    books = [];
+    genreBooks.map(book => {
+      let found = books.some(el => el._id.toString() === book._id.toString());
+      if (!found) books.push(book);
+    });
+    // Search for books by Subject
+  } else if (req.query.subject) {
+    let subjectBooks = [
+      ...(await searchByQuery(req.query.subject, "650", "_0", "a")),
+      ...(await searchByQuery(req.query.subject, "650", "_1", "a"))
+    ];
+    books = [];
+    subjectBooks.map(book => {
+      let found = books.some(el => el._id.toString() === book._id.toString());
+      if (!found) books.push(book);
+    });
   }
+
+  if ((req.query.rangeMin || req.query.rangeMax) && books) {
+    let min = req.query.rangeMin || 0;
+    let max = req.query.rangeMax || 13;
+    let filteredBooks = [];
+
+    for (let i = 0; i < books.length; i++) {
+      for (let j = 0; j < books[i].fields.length; j++) {
+        if (books[i].fields[j]["526"]) {
+          if (books[i].fields[j]["526"]["0_"]) {
+            if (
+              books[i].fields[j]["526"]["0_"]["c"] >= min &&
+              books[i].fields[j]["526"]["0_"]["c"] <= max
+            ) {
+              filteredBooks.push(books[i]);
+            }
+          }
+        }
+      }
+    }
+    books = filteredBooks;
+  }
+
   const data = { books };
   res.send(data);
 });
